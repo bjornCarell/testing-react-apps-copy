8a1756067fd60a42309979d399d61a2c5f962567
Bjorn Carell <bjorn.carell@tink.com>
2021-09-17 07:07:17 +0200

    test(exercise/5): inlineSnapshots

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -6,7 +6,7 @@ import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
 import {setupServer} from 'msw/node'
-import { handlers } from 'test/server-handlers'
+import {handlers} from 'test/server-handlers'
 import Login from '../../components/login-submission'
 
 const buildLoginForm = build({
@@ -36,25 +36,319 @@ test(`logging in displays the user's username`, async () => {
 })
 
 test('not providing password displays error message', async () => {
-  render(<Login />)
-  const {username} = buildLoginForm();
+  const Component = render(<Login />)
+  const {username} = buildLoginForm()
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
-  
-  expect(screen.getByText(/password required/i)).toBeInTheDocument()
+
+  expect(Component).toMatchInlineSnapshot(`
+    Object {
+      "asFragment": [Function],
+      "baseElement": <body>
+        <div>
+          <form>
+            <div>
+              <label
+                for="username-field"
+              >
+                Username
+              </label>
+              <input
+                id="username-field"
+                name="username"
+                type="text"
+              />
+            </div>
+            <div>
+              <label
+                for="password-field"
+              >
+                Password
+              </label>
+              <input
+                id="password-field"
+                name="password"
+                type="password"
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+          <div
+            style="height: 200px;"
+          >
+            <div
+              role="alert"
+              style="color: red;"
+            >
+              password required
+            </div>
+          </div>
+        </div>
+      </body>,
+      "container": <div>
+        <form>
+          <div>
+            <label
+              for="username-field"
+            >
+              Username
+            </label>
+            <input
+              id="username-field"
+              name="username"
+              type="text"
+            />
+          </div>
+          <div>
+            <label
+              for="password-field"
+            >
+              Password
+            </label>
+            <input
+              id="password-field"
+              name="password"
+              type="password"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+        <div
+          style="height: 200px;"
+        >
+          <div
+            role="alert"
+            style="color: red;"
+          >
+            password required
+          </div>
+        </div>
+      </div>,
+      "debug": [Function],
+      "findAllByAltText": [Function],
+      "findAllByDisplayValue": [Function],
+      "findAllByLabelText": [Function],
+      "findAllByPlaceholderText": [Function],
+      "findAllByRole": [Function],
+      "findAllByTestId": [Function],
+      "findAllByText": [Function],
+      "findAllByTitle": [Function],
+      "findByAltText": [Function],
+      "findByDisplayValue": [Function],
+      "findByLabelText": [Function],
+      "findByPlaceholderText": [Function],
+      "findByRole": [Function],
+      "findByTestId": [Function],
+      "findByText": [Function],
+      "findByTitle": [Function],
+      "getAllByAltText": [Function],
+      "getAllByDisplayValue": [Function],
+      "getAllByLabelText": [Function],
+      "getAllByPlaceholderText": [Function],
+      "getAllByRole": [Function],
+      "getAllByTestId": [Function],
+      "getAllByText": [Function],
+      "getAllByTitle": [Function],
+      "getByAltText": [Function],
+      "getByDisplayValue": [Function],
+      "getByLabelText": [Function],
+      "getByPlaceholderText": [Function],
+      "getByRole": [Function],
+      "getByTestId": [Function],
+      "getByText": [Function],
+      "getByTitle": [Function],
+      "queryAllByAltText": [Function],
+      "queryAllByDisplayValue": [Function],
+      "queryAllByLabelText": [Function],
+      "queryAllByPlaceholderText": [Function],
+      "queryAllByRole": [Function],
+      "queryAllByTestId": [Function],
+      "queryAllByText": [Function],
+      "queryAllByTitle": [Function],
+      "queryByAltText": [Function],
+      "queryByDisplayValue": [Function],
+      "queryByLabelText": [Function],
+      "queryByPlaceholderText": [Function],
+      "queryByRole": [Function],
+      "queryByTestId": [Function],
+      "queryByText": [Function],
+      "queryByTitle": [Function],
+      "rerender": [Function],
+      "unmount": [Function],
+    }
+  `)
 })
 
 test('not providing username displays error message', async () => {
-  render(<Login />)
-  const {password} = buildLoginForm();
+  const Component = render(<Login />)
+  const {password} = buildLoginForm()
 
   userEvent.type(screen.getByLabelText(/password/i), password)
   userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
-  
-  expect(screen.getByText(/username required/i)).toBeInTheDocument()
+
+  expect(Component).toMatchInlineSnapshot(`
+    Object {
+      "asFragment": [Function],
+      "baseElement": <body>
+        <div>
+          <form>
+            <div>
+              <label
+                for="username-field"
+              >
+                Username
+              </label>
+              <input
+                id="username-field"
+                name="username"
+                type="text"
+              />
+            </div>
+            <div>
+              <label
+                for="password-field"
+              >
+                Password
+              </label>
+              <input
+                id="password-field"
+                name="password"
+                type="password"
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+          <div
+            style="height: 200px;"
+          >
+            <div
+              role="alert"
+              style="color: red;"
+            >
+              username required
+            </div>
+          </div>
+        </div>
+      </body>,
+      "container": <div>
+        <form>
+          <div>
+            <label
+              for="username-field"
+            >
+              Username
+            </label>
+            <input
+              id="username-field"
+              name="username"
+              type="text"
+            />
+          </div>
+          <div>
+            <label
+              for="password-field"
+            >
+              Password
+            </label>
+            <input
+              id="password-field"
+              name="password"
+              type="password"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+        <div
+          style="height: 200px;"
+        >
+          <div
+            role="alert"
+            style="color: red;"
+          >
+            username required
+          </div>
+        </div>
+      </div>,
+      "debug": [Function],
+      "findAllByAltText": [Function],
+      "findAllByDisplayValue": [Function],
+      "findAllByLabelText": [Function],
+      "findAllByPlaceholderText": [Function],
+      "findAllByRole": [Function],
+      "findAllByTestId": [Function],
+      "findAllByText": [Function],
+      "findAllByTitle": [Function],
+      "findByAltText": [Function],
+      "findByDisplayValue": [Function],
+      "findByLabelText": [Function],
+      "findByPlaceholderText": [Function],
+      "findByRole": [Function],
+      "findByTestId": [Function],
+      "findByText": [Function],
+      "findByTitle": [Function],
+      "getAllByAltText": [Function],
+      "getAllByDisplayValue": [Function],
+      "getAllByLabelText": [Function],
+      "getAllByPlaceholderText": [Function],
+      "getAllByRole": [Function],
+      "getAllByTestId": [Function],
+      "getAllByText": [Function],
+      "getAllByTitle": [Function],
+      "getByAltText": [Function],
+      "getByDisplayValue": [Function],
+      "getByLabelText": [Function],
+      "getByPlaceholderText": [Function],
+      "getByRole": [Function],
+      "getByTestId": [Function],
+      "getByText": [Function],
+      "getByTitle": [Function],
+      "queryAllByAltText": [Function],
+      "queryAllByDisplayValue": [Function],
+      "queryAllByLabelText": [Function],
+      "queryAllByPlaceholderText": [Function],
+      "queryAllByRole": [Function],
+      "queryAllByTestId": [Function],
+      "queryAllByText": [Function],
+      "queryAllByTitle": [Function],
+      "queryByAltText": [Function],
+      "queryByDisplayValue": [Function],
+      "queryByLabelText": [Function],
+      "queryByPlaceholderText": [Function],
+      "queryByRole": [Function],
+      "queryByTestId": [Function],
+      "queryByText": [Function],
+      "queryByTitle": [Function],
+      "rerender": [Function],
+      "unmount": [Function],
+    }
+  `)
 })
